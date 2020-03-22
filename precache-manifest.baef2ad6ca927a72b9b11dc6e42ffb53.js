99c530dd9ea4c28b6341a3d0e2400943c0212a9b
Daniel Sullivan <danielsullivan@Daniels-MacBook-Air.local>
2020-03-21 20:07:20 -0500

    Updates

---
+++ precache-manifest.baef2ad6ca927a72b9b11dc6e42ffb53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4db52e56eb44db0b6bab245112ef70",
+    "url": "/Reminder-App/index.html"
+  },
+  {
+    "revision": "96b096cf0aec0670c781",
+    "url": "/Reminder-App/static/css/main.2d95ed10.chunk.css"
+  },
+  {
+    "revision": "8a43a0221ab657137701",
+    "url": "/Reminder-App/static/js/2.0e545a51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Reminder-App/static/js/2.0e545a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b096cf0aec0670c781",
+    "url": "/Reminder-App/static/js/main.a90eb57b.chunk.js"
+  },
+  {
+    "revision": "5ffb6428e8da561525e5",
+    "url": "/Reminder-App/static/js/runtime-main.f8ff49e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reminder-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
